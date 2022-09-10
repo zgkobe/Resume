3fd002ade57b336275264d26ebd820170ecdfeb9
zgkobe <zgkobezg@gmail.com>
2022-09-09 21:59:22 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var imgg = document.getElementById("imgg");
-imgg.addEventListener("mouseover", function(){
-    this.src="http://clipart-library.com/images_k/shocked-emoji-transparent-background/shocked-emoji-transparent-background-24.png";
-})
-
-imgg.addEventListener("mouseout", function(){
-    this.src="https://ae01.alicdn.com/kf/HTB1fJIALVXXXXb.aXXXq6xXFXXXg/6-Style-Soft-Yellow-Smiley-QQ-Expression-PP-Cotton-Round-Emoji-Cushion-Pillow-For-Home-Decoration.jpg_640x640.jpg";
-})
